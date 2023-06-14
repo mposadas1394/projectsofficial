fad89b3ef0d0976258033c93e9e9049e1ff66721
mposadas1394 <matiasposadas1394@gmail.com>
2023-06-14 18:43:06 -0300

    prepactivity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Matias");
+        Job job1=new Job();
+        job1._jobTitle ="junior";
+        job1._company ="microsoft";
+        job1._startYear=2018;
+        job1._endYear=2020;
+    
+        Job job2=new Job();
+        job2._jobTitle ="senior";
+        job2._company ="apple";
+        job2._startYear=2021;
+        job2._endYear=2022;
+
+        Resume myResume=new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+
+        myResume.Display();
     }
 }
